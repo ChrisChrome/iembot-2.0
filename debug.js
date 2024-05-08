bb642d1afae8a33c087dc52c1af8e6dee0b93f2c
ChrisChrome <christophercookman@gmail.com>
2024-05-08 10:44:34 -0600

    Add debug tool to remove channels

--- debug.js
+++ debug.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client({intents: ["Guilds"]})
 const config = require('./config.json');
 
-categories = ["1237805446019154003", "1237805447004553266", "1237805448128630795", "1237805449089384508"]
+categories = ["1237806700879417416", "1237806701722337451", "1237806702473121853", "1237806703010123857"]
 
 client.once('ready', () => {
 	console.log('Ready!');
